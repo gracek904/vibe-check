93f6c408f251042930ff95aad91fc04f3e228bfb
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-10 05:38:22 -0600

    messed up

--- background.js
+++ background.js
@@ -1,20 +1,28 @@
-alert('calling background')
-let jsonfile = {}
-jsonfile["vibe_check_on"] = JSON.stringify({'true'});;
-chrome.tabs.onCreated.addListener(
-  chrome.storage.local.set(
-    jsonfile,
-    function() {
-      alert('vibe check on set to true')
-    }
-  )
-)
+chrome.browserAction.onClicked.addListener(function() {
+  alert('pressed')
+  coverScreen()
+})
 
-// should not be called when new tab opened
-chrome.tabs.onRemoved.addListener(
-  chrome.storage.local.clear(
-    function() {
-      // alert('cleared storage')
+
+function coverScreen() {
+  let textNodes = deepText(document.getElementsByTagName('body')[0])
+  alert(textNodes.length)
+  for(let i = 0; i < textNodes.length; i++) {
+    // TODO: remove only if hateful
+    textNodes[i].nodeValue = ""
+  }
+}
+
+
+function deepText(node){
+  var A= [];
+  if(node){
+    node= node.firstChild;
+    while(node!= null){
+      if(node.nodeType== 3) A[A.length]=node;
+      else A= A.concat(deepText(node));
+      node= node.nextSibling;
     }
-  )
-)
+  }
+  return A;
+}
