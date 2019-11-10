e6c8cfdb246ad2909b22cf6a19326522abdc2911
= <estherlyoon@gmail.com>
2019-11-09 19:37:31 -0600

    esther

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ let badWords = document.getElementsByTagName("p")
 for (let i = 0; i < badWords.length; i++) {
     // let color = badWords[i].style.color;
     // console.log(color)
+    //change
     // let ourColor = color.length > 0 ? color : "black"
     // badWords[i].style.backgroundColor = color;
     badWords[i].style.visibility = "none"
