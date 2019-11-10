aa06e061ef7f99186440320c78e73fad32a8b150
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-10 10:25:31 -0600

    cleared

--- clear.js
+++ clear.js
@@ -1,11 +1,6 @@
 var textNodes = deepText(document.getElementsByTagName('body')[0])
-// for(let i = 0; i < textNodes.length; i++) {
-//   let txt = textNodes[i].nodeValue
-//   if(txt && txt.length > 10) {
-//     console.log(txt.trim())
-//   }
-// }
-//
+
+
 for(let i = 0; i < textNodes.length; i++) {
   let txt = textNodes[i].textContent
   if(txt && txt.length > 1) {
@@ -20,8 +15,8 @@ for(let i = 0; i < textNodes.length; i++) {
     http.onreadystatechange = function() {
       if (http.readyState === 4 && http.status === 200) {
         response = http.responseText
-        console.log(response)
         if(response['abusive'] > 0.5) {
+          console.log(response + " cleared")
           textNodes[i].nodeValue = ""
         }
       }
@@ -47,23 +42,3 @@ function deepText(node){
   }
   return A;
 }
-
-
-// test
-// let http = new XMLHttpRequest()
-// let params ='text=you f**king a$$hole&api_key=545Ma0mxtAIRv8rC7sbpjgfXe0OHr7v9M2gNjzgeke0';
-//
-// console.log(params)
-// const url = 'https://apis.paralleldots.com/v4/abuse';
-// http.open("POST", url, true);
-// http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//
-// console.log("opened")
-//
-// http.onreadystatechange = function() {
-//   if (http.readyState === 4 && http.status === 200) {
-//     alert(http.responseText);
-//   }
-// }
-// console.log(params);
-// http.send(params);
