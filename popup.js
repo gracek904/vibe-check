1eab08431d177b7477f2d731f3c6d9b6af1786b7
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-10 03:15:16 -0600

    tryna get them(background and popup) to communicate

--- popup.js
+++ popup.js
@@ -1,17 +1,8 @@
-let textNodes = deepText(document.getElementsByTagName('body')[0])
-for(let i = 0; i < textNodes.length; i++) {
-  textNodes[i].nodeValue = ""
-}
-
-function deepText(node){
-  var A= [];
-  if(node){
-    node= node.firstChild;
-    while(node!= null){
-      if(node.nodeType== 3) A[A.length]=node;
-      else A= A.concat(deepText(node));
-      node= node.nextSibling;
-    }
-  }
-  return A;
-}
+window.addEventListener('load', function load(event) {
+  document.getElementById('send').onclick = function() {
+    chrome.tabs.query({"active":true,"lastFocusedWindow": true}, function(tabs) {
+      chrome.tabs.reload()
+      chrome.runtime.sendMessage({greeting: "hello"})
+    });
+  };
+});
